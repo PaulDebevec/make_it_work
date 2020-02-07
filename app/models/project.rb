e1441f4ce9e151da8dacb1f96a78a06c29d04dc3
Paul Debevec <paulmdebevec@gmail.com>
2020-02-07 10:31:47 -0700

    Final commit average years of experience

--- app/models/project.rb
+++ app/models/project.rb
@@ -9,4 +9,13 @@ class Project <ApplicationRecord
   def contestant_count
     contestants.count
   end
+
+  def contestants_average_experience
+    aye = contestants.average(:years_of_experience)
+    aye.to_f.round(2)
+    # I attempted to put round in the view, but I reveived this error...
+    # 'undefined method `round' for nil:NilClass'
+
+    #I also attempted limit(2), but that was
+  end
 end
